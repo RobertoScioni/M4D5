a86a717579bbd683ba6c98a876af0a4dd9bb1468
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-18 14:40:39 +0100

    starte first

---
+++ src/components/WarningSign.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Alert } from "react-bootstrap"
+
+const WarningSign = (props) => {
+	return <Alert variant="danger">{props.text}</Alert>
+}
+
+export default WarningSign
