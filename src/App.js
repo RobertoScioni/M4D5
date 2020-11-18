49a6b10821b4d3ff6b4c28aba968b312c92c8577
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-18 15:07:16 +0100

    ex1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import WarningSign from "./components/WarningSign"
 
 class App extends React.Component {
