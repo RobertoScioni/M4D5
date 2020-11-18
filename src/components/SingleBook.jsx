d1da70a90350fbad1120d18274499179e8f9665d
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-18 16:57:13 +0100

    last two

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,16 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import App from "../App.js"
 import { Card } from "react-bootstrap"
 
+
+// type whatever ={
+// 	text: String,
+// 	onClick(event: React.MouseEvent<Card>): void,
+// };
+
 const SingleBook = (props) => {
+	const [toggled, setToggled] = useState(false);
 	return (
-		<Card style={{ width: "18rem" }}>
+		
+		 <Card  style={{ width: "18rem" }}>
 			<Card.Img variant="top" src={props.book.img} />
 			<Card.Body>
-				<Card.Title>{props.book.title}</Card.Title>
+				<Card.Title onClick={(event)=>{setToggled(!toggled); props.onClick(event);}}>{props.book.title}(toggled: {toggled.toString()})</Card.Title>
 			</Card.Body>
 		</Card>
-	)
+		
+		
+		)
 }
 
 export default SingleBook
